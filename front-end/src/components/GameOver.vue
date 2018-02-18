730e212c2136d2d4db700384829fe49c88e06920
Gabriel Ferreira <gabrielleopferreira@gmail.com>
2018-02-17 23:48:06 -0300

    Add GameOver component

--- front-end/src/components/GameOver.vue
+++ front-end/src/components/GameOver.vue
@@ -1,10 +1,37 @@
 <template>
   <div class="game-over">
-    GAME-OVER
+    <h1 class="title">
+      GAME-OVER
+    </h1>
+
+    <button
+      class="restart-button"
+      type="button"
+      v-on:click="resetGame">
+      Restart Game
+    </button>
   </div>
 </template>
 
 <script>
+export default {
+  name: 'GameOver',
+  data: () => {
+    return {
+      store: this.$store
+    }
+  },
+  computed: {
+    foo () {
+      return this.$store.getters.wordWithAttempts
+    }
+  },
+  methods: {
+    resetGame: function () {
+      this.$store.dispatch('restartGame')
+    }
+  }
+}
 </script>
 
 <style lang="sass">
@@ -12,7 +39,9 @@
   height: 100%;
   width: 100%;
   margin-top: 75px;
-  font-family: 'Alfa Slab One', cursive;
-  font-size: 3em;
-  color: #940000
+
+  .title
+    font-family: 'Alfa Slab One', cursive;
+    font-size: 3em;
+    color: #940000
 </style>
