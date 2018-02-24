cac54b7f06f9db3910f4b7b7d1be20468c97c9dd
Gabriel Ferreira <gabrielleopferreira@gmail.com>
2018-02-24 15:32:14 -0300

    Add style to restart button

--- front-end/src/components/GameOver.vue
+++ front-end/src/components/GameOver.vue
@@ -9,16 +9,18 @@
     <hr>
 
     <div class="row">
-      <h1 class="title">
-        GAME-OVER
-      </h1>
+      <div class="col-sm-12 game-over-pane">
+        <h1 class="title">
+          GAME-OVER
+        </h1>
 
-      <button
-        id="restart-button"
-        type="button"
-        v-on:click="resetGame">
-        Restart Game
-      </button>
+        <button
+          id="restart-button"
+          type="button"
+          v-on:click="resetGame">
+          Restart Game
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -40,21 +42,28 @@ export default {
 </script>
 
 <style lang="sass">
+
 .game-over
   height: 100%;
   width: 100%;
-  margin-top: 75px;
   font-family: 'Alfa Slab One', cursive;
 
-  .title
-    font-size: 3em;
-    color: #940000
+  .game-over-pane
+    margin-top: 125px;
 
-  #restart-button
-    padding: 10px
-    cursor: pointer
+    .title
+      font-size: 4em;
+      color: #940000;
 
-.word-board
-  margin-top: 10px
+    #restart-button
+      padding: 10px;
+      cursor: pointer;
+      border-radius: 10px;
+      border: 0;
+      color: white;
+      transition: background-color 0.15s ease;
+      background-color: #4A0000
 
+    #restart-button:hover
+      background-color: #9E1919
 </style>
