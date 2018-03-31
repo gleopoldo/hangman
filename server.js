c319c255203e226c4794b612b37857d0e1ab6d83
Gabriel Ferreira <gabriel.ferreira@locaweb.com.br>
2018-03-31 12:38:44 -0300

    Require main.js at index.html

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ import path from 'path'
 import express from 'express'
 
 const app = express()
-const HTML_FILE = path.join('index.html')
+const DIST_DIR = path.join(__dirname, 'dist')
+const HTML_FILE = path.join(DIST_DIR, 'index.html')
 const DEFAULT_PORT  = 8080
 
 app.set('port', process.env.PORT || DEFAULT_PORT);
-app.use(express.static('.'));
+app.use(express.static(DIST_DIR));
 
 app.get('*', (req, res) => {
   console.log("Handling request.")
