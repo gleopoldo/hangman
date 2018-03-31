3a9f6ba0d18db15e3d3ce86008e2d9ec99a343e3
Gabriel Ferreira <gabriel.ferreira@locaweb.com.br>
2018-03-31 12:06:06 -0300

    Adjust static files location in express server

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ import path from 'path'
 import express from 'express'
 
 const app = express()
-const DIST_DIR = path.join(__dirname, 'dist')
-const HTML_FILE = path.join(DIST_DIR, 'index.html')
+const HTML_FILE = path.join('index.html')
 const DEFAULT_PORT  = 8080
 
 app.set('port', process.env.PORT || DEFAULT_PORT);
-app.use(express.static(DIST_DIR));
+app.use(express.static('.'));
 
 app.get('*', (req, res) => {
   console.log("Handling request.")
